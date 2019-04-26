61a0eed30213b3e6569d3acd56406a4adccbed85
Kai Huebl <kai@huebl-sgh.de>
2019-04-26 15:04:59 +0200

    Added new class WSOutputMessage

---
+++ src/OpcUaWebServer/WS/WSOutputMessage.h
@@ -0,0 +1,44 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+
+ */
+
+#ifndef __OpcUaWebServer_WSOutputMessage_h__
+#define __OpcUaWebServer_WSOutputMessage_h__
+
+#include <boost/asio.hpp>
+#include <iostream>
+
+namespace OpcUaWebServer
+{
+
+	class WSOutputMessage
+	: public std::ostream
+	{
+	  public:
+		WSOutputMessage(void);
+		~WSOutputMessage(void);
+
+		boost::asio::streambuf& streambuf(void);
+		void streambuf(boost::asio::streambuf& streambuf);
+
+	  private:
+		boost::asio::streambuf streambuf_;
+	};
+
+}
+
+#endif
