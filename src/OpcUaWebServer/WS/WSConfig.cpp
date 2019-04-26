bf033cabf108b48ff043955b1d598172dd79fcfe
Kai Huebl <kai@huebl-sgh.de>
2019-04-26 16:40:07 +0200

    Added new class WSConfig, WSServerBase and WSServer

---
+++ src/OpcUaWebServer/WS/WSConfig.cpp
@@ -0,0 +1,58 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+
+ */
+
+#include "OpcUaWebServer/WS/WSConfig.h"
+
+namespace OpcUaWebServer
+{
+
+	WSConfig::WSConfig(void)
+	: address_("0.0.0.0")
+	, port_(8082)
+	{
+	}
+
+	WSConfig::~WSConfig(void)
+	{
+	}
+
+	void
+	WSConfig::address(const std::string& address)
+	{
+		address_ = address;
+	}
+
+	std::string&
+	WSConfig::address(void)
+	{
+		return address_;
+	}
+
+	void
+	WSConfig::port(uint32_t port)
+	{
+		port_ = port;
+	}
+
+	uint32_t
+	WSConfig::port(void)
+	{
+		return port_;
+	}
+
+}
---
+++ src/OpcUaWebServer/WS/WSConfig.h
@@ -0,0 +1,45 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+
+ */
+
+#ifndef __OpcUaWebServer_WSConfig_h__
+#define __OpcUaWebServer_WSConfig_h__
+
+#include <string>
+
+namespace OpcUaWebServer
+{
+
+	class WSConfig
+	{
+	  public:
+		WSConfig(void);
+		virtual ~WSConfig(void);
+
+		void address(const std::string& address);
+		std::string& address(void);
+		void port(uint32_t port);
+		uint32_t port(void);
+
+	  private:
+		std::string address_;
+		uint32_t port_;
+	};
+
+}
+
+#endif
