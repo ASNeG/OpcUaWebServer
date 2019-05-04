36ee4a9b50897cae05f293d5e3a4851737ba8381
Kai Huebl <kai@huebl-sgh.de>
2019-05-04 12:40:15 +0200

    rework read service test script

--- ftest/WebGateway/AttributeService/Read.py
+++ ftest/WebGateway/AttributeService/Read.py
@@ -87,6 +87,11 @@ c.checkEqual(res['Header']['MessageType'], "GW_ReadResponse")
 c.checkEqual(res['Header']['ClientHandle'], "client-handle")
 c.checkEqual(res['Header']['SessionId'], sessionId)
 c.checkEqual(res['Header']['StatusCode'], "Success")
+c.checkEqual(len(res['Body']['Results']), 2)
+c.checkEqual(res['Body']['Results'][0]['Status'], "Success")
+c.checkEqual(res['Body']['Results'][1]['Status'], "Success")
+c.checkExists(res['Body']['Results'][0]['Value'])
+c.checkExists(res['Body']['Results'][1]['Value'])
 
 
 #
