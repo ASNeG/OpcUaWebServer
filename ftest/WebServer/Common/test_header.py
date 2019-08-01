96b1ab9271b2cc998d7d071501a338b51bd8da69
Aleksey Timin <atimin@gmail.com>
2019-08-01 10:13:30 +0500

    add test for ValueList

--- ftest/WebServer/Common/test_header.py
+++ ftest/WebServer/Common/test_header.py
@@ -3,13 +3,26 @@ import json
 from ..web_server_testcase import WebServerTestCase
 
 
-class TestHistoryReadAttribute(WebServerTestCase):
+class TestCommonCommunication(WebServerTestCase):
 
     def setUp(self):
         WebServerTestCase.setUp(self)
 
     def tearDown(self):
         WebServerTestCase.tearDown(self)
+
+    def test_client_handle(self):
+        msg = {
+            'Header': {
+                'MessageType': 'VALUELIST_REQUEST',
+                'ClientHandle': 'yyyxxxqqq'
+            },
+            'Body': {}
+        }
+
+        self.ws.send(json.dumps(msg))
+        resp = json.loads(self.ws.recv())
+        self.assertEqual(msg['Header']['ClientHandle'], resp['Header']['ClientHandle'])
 
     def test_wrong_message_type(self):
         msg = {
@@ -25,4 +38,3 @@ class TestHistoryReadAttribute(WebServerTestCase):
 
         self.assertEqual('ERROR', resp['Header']['MessageType'])
         self.assertEqual('BadRequestHeaderInvalid', resp['Header']['StatusCode'])
-git 
---
+++ ftest/WebServer/Functions/test_value_list.py
@@ -0,0 +1,31 @@
+import json
+
+from ..web_server_testcase import WebServerTestCase
+
+
+class TestValueList(WebServerTestCase):
+
+    def setUp(self):
+        WebServerTestCase.setUp(self)
+
+    def tearDown(self):
+        WebServerTestCase.tearDown(self)
+
+    def test_value_list(self):
+        msg = {
+            'Header': {
+                'MessageType': 'VALUELIST_REQUEST',
+                'ClientHandle': '1'
+            },
+            'Body': {}
+        }
+
+        self.ws.send(json.dumps(msg))
+        resp = json.loads(self.ws.recv())
+
+
+        self.assertEqual('VALUELIST_RESPONSE', resp['Header']['MessageType'])
+        self.assertEqual(74, len(resp['Body']['Variables']))
+        self.assertIn('Boolean', resp['Body']['Variables'])
+        self.assertIn('Int64', resp['Body']['Variables'])
+        self.assertIn('LocalizedText', resp['Body']['Variables'])
