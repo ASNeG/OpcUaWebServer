dc7d9fffe7eb0ccd23be5b24c7b65ebf7a5278e3
Kai Huebl <kai@huebl-sgh.de>
2019-10-29 21:37:50 +0100

    feat: add new class RequestInfo

---
+++ src/OpcUaWebServer/OpcUaClient/RequestInfo.h
@@ -0,0 +1,42 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+
+ */
+
+#ifndef __OpcUaWebServer_RequestInfo_h__
+#define __OpcUaWebServer_RequestInfo_h__
+
+#include <stdint.h>
+
+namespace OpcUaWebServer
+{
+
+	class RequestInfo
+	{
+	  public:
+		RequestInfo(void);
+		~RequestInfo(void);
+
+		void requestTimeout(uint32_t requestTimeout);
+		uint32_t requestTimeout(void);
+
+	  private:
+		uint32_t requestTimeout_;
+	};
+
+}
+
+#endif
